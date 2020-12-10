72aa2904110b9e56127e41d29bc7948dc4daaf18
Andrew Kurachkin <kurockinandrew@gmail.com>
2020-12-10 20:49:40 +0300

    Finaly 5th

--- 05_Python_challenge.py
+++ 05_Python_challenge.py
@@ -4,16 +4,8 @@ import pickle
 data = ''
 set_url = 'http://www.pythonchallenge.com/pc/def/banner.p'
 setUrl = urllib.request.urlopen(set_url)
-for bline in setUrl.readlines():
-	line = bline.decode('utf8')
-	data = data + line[:-1] #use slising to remove '/n'
 
-#print(data)
+exp_data = pickle.load(setUrl)
 
-with open('pick.p', 'wb') as f:
-	f.write(str(data))
-
-with open('pick.p', 'rb') as f:
-	exp_data = pickle.load(f)
-
-print(exp_data)
+for line in exp_data:
+	print(''.join([k * j for k, j in line]))
